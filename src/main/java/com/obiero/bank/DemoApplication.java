377f74450448eef46d6c5ff843a651b79fb3e401
robertobiero <robertobiero@yahoo.com>
2018-03-20 08:22:06 -0400

    testddd

---
+++ src/main/java/com/obiero/bank/DemoApplication.java
@@ -0,0 +1,12 @@
+package com.obiero.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
